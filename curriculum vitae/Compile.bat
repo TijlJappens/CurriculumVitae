9d47793f47fe79115f0bbe94aed3f303ec1afbc8
Tijl Jappens <tijl.jappens@hotmail.com>
2023-03-21 13:50:50 +0100

    First commit.

---
+++ curriculum vitae/Compile.bat
@@ -0,0 +1,3 @@
+CD /d "%~dp0"
+pdflatex -aux-directory="C:\Users\Tijl Jappens\Desktop\LaTexOutput" -output-directory="C:\Users\Tijl Jappens\Desktop\LaTexOutput" main.tex
+copy main.tex "C:\Users\Tijl Jappens\Desktop\LaTexOutput\main.tex"
